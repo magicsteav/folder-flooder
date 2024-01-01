2c9813307513b76d7baa8c793dd1c83b4169a512
magicsteav <124083313+magicsteav@users.noreply.github.com>
2024-01-01 20:20:45 +0100

    Update hack.bat

--- hack.bat
+++ hack.bat
@@ -1,10 +1,12 @@
 @echo off
 color c
 set /a a=1
+set adresar=%userprofile%\Desktop
 goto hack
 
 :hack
-vytvari se slozka %a%!
-mkdir %userprofile%\Desktop
+echo vytvari se slozka %a%!
+mkdir %adresar%\%a%
 echo slozka %a% byla uspesne vytorena!
 set /a a+=1
+goto hack
