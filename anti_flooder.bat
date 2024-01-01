8d3d32e2a199151b8d8e911317627c464605f8a1
magicsteav <124083313+magicsteav@users.noreply.github.com>
2024-01-01 20:39:42 +0100

    Update anti_flooder.bat

--- anti_flooder.bat
+++ anti_flooder.bat
@@ -1,18 +1,23 @@
 @echo off
-color y
-set /a a=1
-set adresar=%userprofile%\Desktop
-goto zachrana
+color 0E
+setlocal enabledelayedexpansion
 
+rem Nastavení proměnných
+set "a=1"
+set "adresar=%userprofile%\Desktop"
+
+rem Zpráva o začátku procesu
+echo Proces odstranění složek na ploše začíná.
+
+rem Procházení složek a odstraňování
 :zachrana
-if exist %adresar%\%a% (
-del %adresar%\%a% 
-set /a a+=1
-goto zachrana 
-)
-
-else (
-echo vsechny slozky byly spravne odstraneny!
-pause
-exit
+if exist "%adresar%\!a!" (
+    rd /s /q "%adresar%\!a!"
+    echo Složka !a! byla odstraněna.
+    set /a a+=1
+    goto zachrana
+) else (
+    echo Všechny složky byly úspěšně odstraněny.
+    pause
+    exit /b
 )
